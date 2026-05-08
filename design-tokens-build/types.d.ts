1308f495b35aad3b7924aee21a32f6f808edc2b0
Ubuntu <jorgeneilertsen@mipro-dev-ubuntu-jorgen.s5e12mj4tv4e1pmrjdx1cxyk2g.gvxx.internal.cloudapp.net>
2026-05-08 08:08:31 +0000

    bygget designsystemet

---
+++ design-tokens-build/types.d.ts
@@ -0,0 +1,19 @@
+/* build: v1.14.0 */
+import type {} from '@digdir/designsystemet-types';
+
+// Augment types based on theme
+declare module '@digdir/designsystemet-types' {
+  export interface ColorDefinitions {
+    skarstind: never;
+    friskefakta: never;
+    mipro: never;
+    norpuls: never;
+    neutral: never;
+  }
+  export interface SeverityColorDefinitions {
+    info: never;
+    success: never;
+    warning: never;
+    danger: never;
+  }
+}
